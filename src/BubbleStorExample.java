39edb182829a3f75d83e7be0c2197af8e9da56c0
Karol <karolzymkowski1990@gmail.com>
2023-09-30 20:17:46 +0200

    Initial commit

---
+++ src/BubbleStorExample.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class BubbleStorExample {
+    public static void main(String[] args) {
+        int num, i, j, temp;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter the number of integers to sort:");
+        num = input.nextInt();
+
+        int array[] = new int[num];
+
+        System.out.println("Enter " + num + " integers: ");
+
+        for (i = 0; i < num; i++)
+            array[i] = input.nextInt();
+
+        for (i = 0; i < (num - 1); i++) {
+            for (j = 0; j < (num - 1); i++) {
+                if (array[j] > array[j + 1]) {
+                    temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+        System.out.println("Sorted list of integers:");
+
+        for (i = 0; i < num; i++)
+            System.out.println(array[i]);
+    }
+}
